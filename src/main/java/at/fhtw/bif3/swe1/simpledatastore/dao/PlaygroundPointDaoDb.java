efdf8b2ce94d5aaec83825ea1f403315db93352d
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2020-12-02 16:23:07 +0100

    create, update, delete implemented

--- src/main/java/at/fhtw/bif3/swe1/simpledatastore/dao/PlaygroundPointDaoDb.java
+++ src/main/java/at/fhtw/bif3/swe1/simpledatastore/dao/PlaygroundPointDaoDb.java
@@ -2,10 +2,8 @@ package at.fhtw.bif3.swe1.simpledatastore.dao;
 
 import at.fhtw.bif3.swe1.simpledatastore.model.PlaygroundPointData;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.security.cert.CertificateParsingException;
+import java.sql.*;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -32,19 +30,20 @@ public class PlaygroundPointDaoDb implements Dao<PlaygroundPointData> {
 
     /**
      * initializes the database with its tables
-     * PostgreSQL documentation: https://www.postgresqltutorial.com/postgresql-create-table/
      */
+    // PostgreSQL documentation: https://www.postgresqltutorial.com/postgresql-create-table/
     public static void initDb() {
         // re-create the database
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/","postgres","") ) {
+        try (Connection connection = getConnection("")) {
             executeSql(connection, "DROP DATABASE simpledatastore", true );
-            executeSql(connection,  "CREATE DATABASE simpledatastore" );
+            executeSql(connection,  "CREATE DATABASE simpledatastore", true );
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
         // create the table
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/simpledatastore","postgres","") ) {
+        // PostgreSQL documentation: https://www.postgresqltutorial.com/postgresql-create-table/
+        try (Connection connection = getConnection()) {
             executeSql(connection,  """
                 CREATE TABLE IF NOT EXISTS PlaygroundPoints (
                     fId VARCHAR(50) NOT NULL,
@@ -76,20 +75,71 @@ public class PlaygroundPointDaoDb implements Dao<PlaygroundPointData> {
 
     @Override
     public void save(PlaygroundPointData playgroundPointData) {
-
+        try (Connection connection = getConnection() ) {
+            PreparedStatement statement = connection.prepareStatement("""
+                INSERT INTO playgroundpoints 
+                (fid, objectid, shape, anlname, bezirk, spielplatzdetail, typdetail, seannocaddata) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+                """ );
+            statement.setString(1, playgroundPointData.getFId() );
+            statement.setInt( 2, playgroundPointData.getObjectId() );
+            statement.setString(3, playgroundPointData.getShape() );
+            statement.setString( 4, playgroundPointData.getAnlName() );
+            statement.setInt( 5, playgroundPointData.getBezirk() );
+            statement.setString( 6, playgroundPointData.getSpielplatzDetail() );
+            statement.setString( 7, playgroundPointData.getTypDetail() );
+            statement.setString( 8, playgroundPointData.getSeAnnoCadData() );
+            statement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void update(PlaygroundPointData playgroundPointData, String[] params) {
-
+        try (Connection connection = getConnection() ) {
+            PreparedStatement statement = connection.prepareStatement("""
+                UPDATE public.playgroundpoints 
+                SET fid = ?, shape = ?, anlname = ?, bezirk = ?, spielplatzdetail = ?, typdetail = ?, seannocaddata = ? 
+                WHERE objectid = ?;
+                """);
+            statement.setString(1, playgroundPointData.getFId() );
+            statement.setString(2, playgroundPointData.getShape() );
+            statement.setString( 3, playgroundPointData.getAnlName() );
+            statement.setInt( 4, playgroundPointData.getBezirk() );
+            statement.setString( 5, playgroundPointData.getSpielplatzDetail() );
+            statement.setString( 6, playgroundPointData.getTypDetail() );
+            statement.setString( 7, playgroundPointData.getSeAnnoCadData() );
+            statement.setInt( 8, playgroundPointData.getObjectId() );
+            statement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void delete(PlaygroundPointData playgroundPointData) {
-
+        try (Connection connection = getConnection() ) {
+            PreparedStatement statement = connection.prepareStatement("""
+                DELETE FROM public.playgroundpoints 
+                WHERE objectid = ?;
+                """);
+            statement.setInt( 1, playgroundPointData.getObjectId() );
+            statement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 
+    private static Connection getConnection(String database) throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + database, "postgres", "");
+    }
+
+    private static Connection getConnection() throws SQLException {
+        return getConnection("simpledatastore");
+    }
+    
     private static boolean executeSql(Connection connection, String sql, boolean ignoreIfFails) throws SQLException {
         try {
             Statement statement = connection.createStatement();
